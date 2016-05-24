245fd97c8bd5e99b33bf5cd33f83d43b07f85ae3
BlackyFox <puissant.antoine@gmail.com>
2016-05-24 17:41:43 +0200

    provisionning the directory and removing test calls

--- derivPass.py
+++ derivPass.py
@@ -3,6 +3,7 @@
 
 """
 This script aim is to derivate some strings (passwords).
+Author: BlackyFox
 """
 
 import os
@@ -12,32 +13,32 @@ import string
 import codecs
 
 def init():
-    a = ["A", u"@", u"à", u"â", u"ä", u"Ä", u"Â"]
-    b = ["B"]
-    c = ["C", u"ç"]
-    d = ["D"]
-    e = ["E", u"é", u"è", u"ê", u"ë", u"€", u"3", u"Ê", u"Ë"]
-    f = ["F"]
-    g = ["G"]
-    h = ["H"]
-    i = ["I", "1", u"î", u"ï", u"Î", u"Ï"]
-    j = ["J"]
-    k = ["K"]
-    l = ["L", "1"]
-    m = ["M"]
-    n = ["N"]
-    o = ["O", u"ô", u"ö", u"Ô", u"Ö", u"0"]
-    p = ["P"]
-    q = ["Q"]
-    r = ["R"]
-    s = ["S", u"$"]
-    t = ["T"]
-    u = ["U", u"ù", u"û", u"ü", u"Û", u"Ü"]
-    v = ["V", "\/"]
-    w = ["W", "\/\/"]
-    x = ["X"]
-    y = ["Y"]
-    z = ["Z"]
+    a = [u"A", u"@", u"à", u"â", u"ä", u"Ä", u"Â", u"4"]
+    b = [u"B", u"8", r"|3", r"|o"]
+    c = [u"C", u"ç", r"(", r"<"]
+    d = [u"D", r"|)"]
+    e = [u"E", u"é", u"è", u"ê", u"ë", u"€", u"3", u"Ê", u"Ë"]
+    f = [u"F", r"|="]
+    g = [u"G", u"6"]
+    h = [u"H", r"|-|"]
+    i = [u"I", u"1", u"î", u"ï", u"Î", u"Ï", u"|", u"!"]
+    j = [u"J", r"_|"]
+    k = [u"K", r"|<"]
+    l = [u"L", u"1"]
+    m = [u"M"]
+    n = [u"N"]
+    o = [u"O", u"ô", u"ö", u"Ô", u"Ö", u"0"]
+    p = [u"P"]
+    q = [u"Q"]
+    r = [u"R"]
+    s = [u"S", u"$", u"5"]
+    t = [u"T", u"7"]
+    u = [u"U", u"ù", u"û", u"ü", u"Û", u"Ü", r"|_|"]
+    v = [u"V", r"\/"]
+    w = [u"W", r"\/\/"]
+    x = [u"X", u"><"]
+    y = [u"Y"]
+    z = [u"Z", u"2"]
 
     rr = []
     rr.append(a)
@@ -89,18 +90,15 @@ def main(arguments):
         with codecs.open(args.output, 'w', 'utf-8') as ff:
             for i in s:
                 ff.write(i)
-    #os.remove(fi)
+    os.remove(fi)
 
 def deriv(s, ss, tab, begin, f):
     for i in range(begin,len(s)):
         for ii in tab[string.lowercase.index(s[i])]:
             ss = ss[:i] + ii + ss[i + 1:]
-            #print(ss)
             f.write(ss + "\n")
-            #print("Begin: " + str(begin) + " / Len(s): " + str(len(s)))
             if (begin + 1) < (len(s)):
                 deriv(s, ss, tab, begin + 1, f)
-            #ss = s
         ss = s
 
 def _deriv(s, p, i, tab):
